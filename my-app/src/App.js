5d3248842f3e8bb6a9b7557cbac5e43200e8203b
cynthwjwu <cynthwjwu@gmail.com>
2018-03-06 16:43:31 -0600

    set up headings

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
+import './Style.css'
 
 class App extends Component {
   render() {
     return (
       <div classname="App">
-      Test
+        <h1>One on One Tutoring</h1>
+        <h1>Schedule a Tutor</h1>
+        <h1>About Us</h1>
+        <h1>Our Story</h1>
+        <h1>Our Tutors</h1>
+        <h1>Our Clients Testimonials</h1>
+        <h1>Philanthropy</h1>
+        <h1>Join Our Mailing List</h1>
+
       </div>
+
     );
   }
 }
