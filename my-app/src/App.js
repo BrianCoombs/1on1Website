63b788614c524cdebd265dde7c6261d38f9887af
Bryan <b.yao@utexas.edu>
2018-03-11 21:04:51 -0500

    prototype for testimonials

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,47 @@ import React, {Component} from 'react';
 import './Style.css'
 
 class App extends Component {
+  constructor(props, context){
+    super(props, context);
+    this.prv = this.prv.bind(this);
+    this.nxt = this.nxt.bind(this);
+    this.state = {
+            page1: true,
+            page2: false,
+            page3: false,
+            page4: false
+       }
+    console.log('Initializing')
+  }
+  nxt() {
+	if (this.state.page1){  
+      this.setState({page1:false, page2: true});
+	}
+	else if (this.state.page2){  
+	      this.setState({page2:false, page3: true});
+	}
+	else if (this.state.page3){  
+	      this.setState({page3:false, page4: true});
+	}
+  }
+  prv() {
+	if (this.state.page2){  
+	      this.setState({page2:false, page1: true});
+		}
+		else if (this.state.page3){  
+		      this.setState({page3:false, page2: true});
+		}
+		else if (this.state.page4){  
+		      this.setState({page4:false, page3: true});
+		}
+  }
   render() {
+	let page1 = (this.state.page1) ? "block" : "none"
+	let page2 = (this.state.page2) ? "block" : "none"
+	let page3 = (this.state.page3) ? "block" : "none"
+	let page4 = (this.state.page4) ? "block" : "none"
+	let button1 = (this.state.page1) ? "none" : "block"
+	let button2 = (this.state.page4) ? "none" : "block"	
     return (
       <div classname="App">
         <h1>One on One Tutoring</h1>
@@ -11,6 +51,26 @@ class App extends Component {
         <h1>Our Story</h1>
         <h1>Our Tutors</h1>
         <h1>Our Clients Testimonials</h1>
+	<div class="testimonial">
+	    <button style={{display: button1}} class="button button1" onClick={this.prv}><span>Prev </span></button>
+	    <button style={{display: button2}} class="button button2" onClick={this.nxt}><span>Next </span></button>
+	    <p style={{display: page1}} class="test1 review1">"This place is great!"</p>
+        <p style={{display: page1}} class="test1 person1">-Distinguished Customer</p>
+	    <img style={{display: page1}} class="test1 img1" src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjI0MTg3MzI0M15BMl5BanBnXkFtZTcwMzQyODU2Mw@@._V1_UY317_CR10,0,214,317_AL_.jpg" alt="1-time Oscar winner"/>
+	    <img style={{display: page1}} class="test1 page1" src="https://i.imgur.com/exar0UV.png" alt=""/>
+	    <p style={{display: page2}} class="test2 review2">"I am Yale Patt!"</p>
+        <p style={{display: page2}} class="test2 person2">-Yale Patt</p>
+	    <img style={{display: page2}} class="test2 img2" src="https://www.ithistory.org/sites/default/files/honor-roll/Yale%20N.%20Patt.jpg" alt="Distinguished professor"/>
+	    <img style={{display: page2}} class="test2 page2" src="https://i.imgur.com/Orw0hDN.png" alt=""/>
+	    <p style={{display: page3}} class="test3 review3">"3 Michelin Stars"</p>
+        <p style={{display: page3}} class="test3 person3">-Celebrity Chef</p>
+	    <img style={{display: page3}} class="test3 img3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Gordon_Ramsay.jpg/330px-Gordon_Ramsay.jpg" alt="chef"/>
+	    <img style={{display: page3}} class="test3 page3" src="https://i.imgur.com/F0C1rkf.png" alt=""/>
+	    <p style={{display: page4}} class="test4 review4">"5 stars out of 5"</p>
+        <p style={{display: page4}} class="test4 person4">-Princeton Review</p>
+	    <img style={{display: page4}} class="test4 img4" src="https://i.imgur.com/bYHXqfV.png" alt="stars"/>
+	    <img style={{display: page4}} class="test4 page4" src="https://i.imgur.com/c7agBZW.png" alt=""/>
+	</div>
         <h1>Philanthropy</h1>
         <h1>Join Our Mailing List</h1>
 
