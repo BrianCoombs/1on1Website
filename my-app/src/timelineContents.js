1a61815bec5152e83736a07f0c8da4fa0f518a7c
vero <veronicasun@utexas.edu>
2018-03-22 09:37:11 -0500

    using react to make timeline

---
+++ my-app/src/timelineContents.js
@@ -0,0 +1,29 @@
+/*paragraph descriptions*/
+const p1 = 'First event description - 1on1Tutoring was founded on January 1st of 2010.....';
+const p2 = 'This is what the second event will say';
+const p3 = 'Third paragraph description';
+
+module.exports = [
+  {
+    date: '01/01/2010',
+    title: 'Origins',
+    subtitle: '1on1 Tutoring wanted to help students'
+    content: p1
+  },
+
+  {
+    date: '02/01/2011',
+    title: 'Event2',
+    subtitle: 'subEvent description 2'
+    content: p2
+  },
+
+  {
+    date: '03/01/2014',
+    title: 'Event3',
+    subtitle: 'subEvent description 3'
+    content: p3
+  }
+
+
+]
