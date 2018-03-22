d96ef92d9f7081c579dc918fb7d18ed4f8b0af10
vero <veronicasun@utexas.edu>
2018-03-22 10:18:44 -0500

    created timeline not using react pkg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,9 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
 
-import eventInfo from './timelineContents.js'
-import HorizontalTimelineContent from '../HorizontalTimeline/HorizontalTimelineContent'
 import './Style.css'
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {value:0, previous:0};
-  }
-
-  componentWillMount() {
-    this.data = eventInfo.map((events, index) => {
-      return ({
-        date: events.date,
-        component: (
-          <div className = 'container' key = {index} >
-            <h1> {events.title}</h1>
-            <h2> {events.subtitle} </h2>
-            <hr />
-            <p> {events.content} </p>
-            <hr />
-          </div>
-        )
-      });
-    });
-  }
 
   render() {
     return (
@@ -35,8 +11,50 @@ class App extends Component {
         <h1>One on One Tutoring</h1>
         <h1>Schedule a Tutor</h1>
         <h1>About Us</h1>
+
         <h1>Our Story</h1>
-          <HorizontalTimelineContent content={this.data} />
+          <section class="section intro">
+            <div class="container">
+            </div>
+          </section>
+
+          <section class="timeline">
+            <ol>
+              <li>
+                <div>
+                  <time>01/01/2010</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                </div>
+              </li>
+              <li>
+                <div>
+                  <time>02/03/2011</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                </div>
+              </li>
+              <li>
+                <div>
+                  <time>03/03/2013</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                </div>
+              </li>
+              <li>
+                <div>
+                  <time>03/03/2014</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                </div>
+              </li>
+              <li>
+                <div>
+                  <time>03/03/2015</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                </div>
+              </li>
+              <li>
+                <div>
+                  <time>01/03/2016</time> In mattis elit vitae odio posuere, nec maximus massa varius. Suspendisse varius volutpat mattis. Vestibulum id magna est.
+                </div>
+              </li>
+              <li></li>
+            </ol>
+
+          </section>
+
         <h1>Our Tutors</h1>
         <h1>Our Clients Testimonials</h1>
         <h1>Philanthropy</h1>
