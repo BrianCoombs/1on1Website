bbce556e3b229acd892585034205c6daefeefd0b
cynthwjwu <cynthwjwu@gmail.com>
2018-04-25 23:35:17 -0500

    acuity working

--- my-app/src/components/ScheduleTutor.js
+++ my-app/src/components/ScheduleTutor.js
@@ -6,6 +6,8 @@ class ScheduleTutor extends Component {
     return (
       <div>
         <h1 id="schedule">Schedule a Tutor</h1>
+        <iframe src="https://app.acuityscheduling.com/schedule.php?owner=13918864" width="100%" height="800" frameBorder="0"></iframe>
+        <script src="https://d3gxy7nm8y4yjr.cloudfront.net/js/embed.js" type="text/javascript"></script>
       </div>
     );
   }
