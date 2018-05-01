972b484ff0d5eef088d583f39855ba356dc6c3d5
Bryan <b.yao@utexas.edu>
2018-05-01 15:23:15 -0500

    updated test

--- my-app/__tests__/NameForm.test.js
+++ my-app/__tests__/NameForm.test.js
@@ -11,16 +11,16 @@ it('should show initial states', () => {
   const component = shallow(
     <NameForm />
   );
-  expect(component.state().schedulesHidden).toEqual(true);
+  expect(component.state().schedulesHidden).toEqual(false);
 });
 
 it('test submit', () => {
   const component = shallow(
     <NameForm />
   );
-  expect(component.state().schedulesHidden).toEqual(true);
-  component.find('form').simulate('submit', { preventDefault () {} });
   expect(component.state().schedulesHidden).toEqual(false);
+  component.find('form').simulate('submit', { preventDefault () {} });
+  expect(component.state().schedulesHidden).toEqual(true);
 });
 
 /*
