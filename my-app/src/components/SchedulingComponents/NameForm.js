fd253080b03e7f77fc2316280c5954425023f410
Brian Coombs <bjcoombs1@gmail.com>
2018-05-02 15:00:27 -0500

    request api working for regular requests, still need to get express server working for Acuity requests...

--- my-app/src/components/SchedulingComponents/NameForm.js
+++ my-app/src/components/SchedulingComponents/NameForm.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
-
 import AllSchedules from './AllSchedules'
+import acuityscheduling from 'acuityscheduling'
+
+
+const request = require('request');
+
+//const express = require('express');
+//const app = express();
+//const Acuity = require('acuityscheduling');
+
 export default class NameForm extends Component {
   constructor(props) {
     super(props);
@@ -26,8 +34,30 @@ export default class NameForm extends Component {
     })
     */
 
+    request('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', { json: true }, (err, res, body) => {
+      if (err) { return console.log(err); }
+      alert(body.url);
+      alert(body.explanation);
+    });
+
+/* Attempt at Express
+    app.get('/', function (req, res) {
+      const userId = "13918864";
+      const apiKey = "25f69d363eda2abc42d5471c13c5d493";
+      const acuity = Acuity.basic({
+        userId: userId,
+        apiKey: apiKey
+      });
+      acuity.request('/appointments', function (err, res, appointments) {
+        if (err) { return console.error(err); }
+        alert(appointments);
+      });
+    });
+
+    app.listen(4000, () => console.log('Example app listening on port 4000!'))
+*/
     this.setState({
-      schedulesHidden: true
+      schedulesHidden: !(this.state.schedulesHidden)
     })
 
     //alert('A name was submitted: ' + this.state.name + ' with zip of ' + this.state.zip + ' with requested subject of ' + this.state.subject);
