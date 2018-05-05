5cc64730dbaad8672be9519a15eed5a4defef247
purahmanian <31709752+purahmanian@users.noreply.github.com>
2018-05-04 20:56:22 -0500

    attempted mailing list

--- my-app/client/src/components/MailingList.js
+++ my-app/client/src/components/MailingList.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
-//import axios from 'axios'
-//import curl from 'curl'
+const Mailchimp = require('mailchimp-api-js');
+const mc = new Mailchimp('1on1Tutoring', '5265796dfdeb31263a5c23cc561941d5-us15');
+var request = require('superagent');
+
+
 
 class MailingList extends Component {
 
@@ -14,57 +17,47 @@ class MailingList extends Component {
     this.email = this.refs.input.value;
   }
 
-  addToMailList() {
-    //event.preventDefault();
-
-    //curl.post('https://us16.api.mailchimp.com/3.0/lists/f403e6a812/members',{email_address:"purahmanian@gmail.com", status:"subscribed"});
-
-/*
-
-    axios.post(`https://1on1Tutoring:5265796dfdeb31263a5c23cc561941d5-us15@us16.api.mailchimp.com/3.0/lists/f403e6a812/members`, {
+  addToMailList = async () => {
+    const body = {
       email_address: 'purahmanian@gmail.com',
       status: 'subscribed'
+    };
+    var endpoint = 'https://us15.api.mailchimp.com/3.0/lists/f403e6a812/members';
+    var req = request(endpoint, function(error, response, body){
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      console.log('body:', body); // Print the HTML for the Google homepage.
+      alert("hi");
     })
-    .then(function (response) {
+          .set('content-type', 'application/json')
+          .auth('1on1Tutoring', '5265796dfdeb31263a5c23cc561941d5-us15', false);
+
+    req.send(body);
+/*
+
+
+      const response = await fetch('/api/mailchimp');
+
       console.log(response);
-      alert("response:\n" + response);
-    })
-    .catch(function (error) {
-      console.log(error);
-      alert("error:\n" + error);
-    });
+      alert(response);
+      const body = await response.json();
 
 
-    axios({
-      url:'https://us16.api.mailchimp.com/3.0/lists/f403e6a812/members',
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        email_address: this.email,
-        status: "subscribed",
-      }),
-      auth: {
-        'user': '1on1Tutoring',
-        'pass': '5265796dfdeb31263a5c23cc561941d5-us15'
-      }
-    })
+      if (response.status !== 200) throw Error(body.message);
 
-    axios({
-      url: 'https://usX.api.mailchimp.com/3.0/lists/f403e6a812/members',
-      user: '1on1Tutoring:5265796dfdeb31263a5c23cc561941d5-us15',
-      header: 'content-type: application/json',
-      data: user
-    }).then(res => {
-       console.log(res);
-       console.log(res.data);
-     })*/
+
+
+
+      onSubmit={this.addToMailList.bind(this)}
+
+*/
+
 
   }
 
   render() {
     return (
+      
       <div>
         <h1>Join Our Mailing List</h1>
         <div class="mailingList">
@@ -73,13 +66,17 @@ class MailingList extends Component {
 
           <p class="mailing">For discounts and free tutoring updates, we promise we hate spam as much as you do.</p>
 
-          <form id="hello" action="" onSubmit={this.addToMailList.bind(this)} >
+          <form id="hello" action="/api/mailchimp" method="POST"  >
+
+
             <input
               ref="input"
-              onChange={this.update.bind(this)}
+              type="text"
+              name="email"
+              id="email"
               //addToMailList = {this.addToMailList.bind(this))}
-              class="emailInput" type="text" name="emailAddress"
-            />{this.email}
+              class="emailInput"
+            />
             <input class="emailSubmit" type="submit" value="Submit"/>
 
           </form>
